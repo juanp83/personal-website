bc7c5ffa85c26d3a1271b1077fc09634bf98fb2b
Juan Portillo <juan@goowee.io>
2022-11-29 15:31:13 -0700

    get mantine working, setup hero

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import SimpleHeader from '../components/SimpleHeader';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const links = [{link: '/', label: 'Home'}]
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  console.log({Component})
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <SimpleHeader links={links} />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
 }
-
-export default MyApp
